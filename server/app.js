7af41410e19515b6930998d6183b7ccd9ae89a41
Giridharan <giridharanias7@gmail.com>
2025-01-05 12:39:13 +0530

    Completed the webrouter db setup successfully using sequelise as orm

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const cros = require('cros');
+const cors = require('cors');
 const webRouter = require('./routes/account');
 const reviewRouter = require('./routes/profile');
 
+const sequelize = require('./db');
 
 const app = express();
 
-app.use(cros());
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/", webRouter);
 app.use("/", reviewRouter);
+
+app.listen(3000, () => {
+    console.log("Server running on the port 3000")
+
+    sequelize.authenticate()
+        .then(() => console.log("Database is connected successfully"))
+        .catch((error) => console.log("Error while connecting to the database", error))
+})
---
+++ server/db.js
@@ -0,0 +1,22 @@
+const {sequelize, Datatypes} = require('sequelize');
+const AccountModel = require('./models/account');
+const UserOtpModel = require('./models/user_otp');
+
+const sequelize = new sequelize('TakeReview', 'Giridharan', 'Giridev1729!', {
+    host: '127.0.0.1',
+    dialect: 'postgres'
+})
+
+const Account = AccountModel(sequelize, Datatypes);
+const UserOtp = AccountModel(sequelize, Datatypes);
+
+UserOtp.belongsTo(Account, { foreignKey: 'account_id' });
+
+sequelize.sync({ alter:true })
+    then(() => {
+        console.log("Database is connected");
+    }).catch((error) => {
+        console.log("Error while connecting to database", error);
+    });
+
+module.exports = { sequelize, Account, UserOtp }
