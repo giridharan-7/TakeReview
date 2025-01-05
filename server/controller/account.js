004df4cc1ba35c85683e79d92ec0685f4ab21f08
Giridharan <giridharanias7@gmail.com>
2025-01-05 12:22:43 +0530

    Created the account and user_otp model

--- server/controller/account.js
+++ server/controller/account.js
@@ -1,5 +1,21 @@
 
 const signup = async (req, res) => {
+    const userName = req.username;
+    const email = req.email;
+    const password = req.password;
+
+    // add the user in db
+
+    
+
+
+    // send verification otp to the user email
+
+
+    // store the otp in db
+
+
+    // return 
     
 }
 
