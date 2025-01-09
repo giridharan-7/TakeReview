f95e60b5ec5c02182fb0f071f910141a9471bbef
Giridharan <giridharanias7@gmail.com>
2025-01-09 18:03:20 +0530

    Established Rabbitmq connection and pushed the job to queue

--- server/controller/platform.js
+++ server/controller/platform.js
@@ -1,14 +1,32 @@
 const { Platform, Account } = require("../models/db");
 
+const RABBITMQ_URL = 'amqp://localhost';  // rabitmq url
+const QUEUE_NAME = 'platform_jobs';
+
+async function connectRabbitMq() {
+    try {
+        const connection = await amqp.connect(RABBITMQ_URL);
+        const channel = await connection.createChannel();
+        await channel.assertQueue(QUEUE_NAME, { durable: true });
+        return channel;
+    } catch (error) {
+        console.error('RabbitMQ Connection Error', error);
+        throw error
+    }
+} 
+
 
 const addPlatform = async (req, res) => {
     const { platformName, platformLink, instantCount } = req.body;
 
     const { api_key } = req.headers['api_key'];
 
+    if(!api_key) {
+        return res.json({ success: true, message: 'Missing api key in headers'});
+    }
+
     try {
-        // if api key is valid then add the platform_name and platform_link to platform table
-    
+            
         const account = await Account.findOne({
             where: { api_key }
         })
@@ -19,19 +37,35 @@ const addPlatform = async (req, res) => {
 
         const platform = await Platform.create(
             {
-                account_id: account,
+                account_id: account.id,
                 platform_name: platformName,
                 platform_link: platformLink,
                 instant_count: instantCount,
             }
         )
 
-        // add the platform as a job to the queue
+        const channel = await connectRabbitMq();
+
+        const jobPayload = {
+            platformId: platform.id,
+            platformName: platform.platform_name,
+            platformLink: platform.platform_link,
+            accountId: account.id,
+            instantCount: platform.instant_count,
+        }
+
+        channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(jobPayload)), {
+            persistent: true,
+        });
+        console.log('Job added to queue:', jobPayload);
+
+        // add the platform as a job rabbitmq to the queue
 
         // return platform id
 
     } catch (error) {
-        
+        console.error('Error in adding platform link to database', error);
+        throw error
     }
 
 }
